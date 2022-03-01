24777f8a58217ae82680c8a009b81e2a279596f4
Joao Gomes <joaogomes@pop-os.localdomain>
2022-03-01 18:05:16 -0300

    Criado DTO e Entity referente a Product

---
+++ src/main/java/com/senai/market/model/entity/Product.java
@@ -0,0 +1,25 @@
+package com.senai.market.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String description;
+    private String price;
+
+    public Product() {
+
+    }
+}
