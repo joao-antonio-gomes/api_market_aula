b902daacb817fe5e9938e02666abfae370037fee
João Gomes <devjoaoantoniogomes@gmail.com>
2022-03-01 22:45:16 -0300

    Adicionado logger

--- src/main/java/com/senai/market/service/ProductService.java
+++ src/main/java/com/senai/market/service/ProductService.java
@@ -6,11 +6,18 @@ import com.senai.market.model.entity.Category;
 import com.senai.market.model.entity.Product;
 import com.senai.market.model.repository.ProductRepository;
 import lombok.SneakyThrows;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class ProductService {
+
+    private final static Logger logger = Logger.getLogger(String.valueOf(ProductService.class));
 
     @Autowired
     private ProductRepository productRepository;
@@ -21,15 +28,19 @@ public class ProductService {
     @SneakyThrows
     public void create(ProductDto productDto) {
         if (productDto.getName().isEmpty()) {
+            logger.log(Level.WARNING, "Usuário tentou cadastrar produto sem nome");
             throw new ProductException("Nome do produto é obrigatório!");
         }
         if (productDto.getDescription().isEmpty()) {
+            logger.log(Level.WARNING, "Usuário tentou cadastrar produto sem descrição");
             throw new ProductException("Descrição do produto é obrigatório!");
         }
         if (productDto.getPrice().isNaN() || productDto.getPrice() == null) {
+            logger.log(Level.WARNING, "Usuário tentou cadastrar produto sem preço ou preço inválido");
             throw new ProductException("Preço do produto deve ser número e é obrigatório!");
         }
         if (productDto.getCategory() == null) {
+            logger.log(Level.WARNING, "Usuário tentou cadastrar produto sem categoria");
             throw new ProductException("Categoria do produto é obrigatório!");
         }
         Category category = categoryService.getCategoryByName(productDto.getCategory().getName());
@@ -38,6 +49,8 @@ public class ProductService {
                 productDto.getDescription(),
                 productDto.getPrice(),
                 category);
-        productRepository.save(product);
+        Product productSaved = productRepository.save(product);
+
+        logger.log(Level.INFO, "Usuário cadastrou produto id " + productSaved.getId());
     }
 }
